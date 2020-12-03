80ef71d130affdb26b08d4776228ae6b7926d23b
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2020-12-03 16:17:02 +1100

    2020 day 3

---
+++ 2020/day03.py
@@ -0,0 +1,22 @@
+data = [x for x in open('inputs/input03.txt').read().strip().split('\n')]
+
+
+def count_trees(right, down):
+    x, y = 0, 0
+    tree_count = 0
+    while y < len(data):
+        if data[y][x] == '#':
+            tree_count += 1
+        x += right
+        x %= len(data[y])
+        y += down
+    return tree_count
+
+
+total_trees = 1
+
+for right_count, down_count in [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]:
+    total_trees *= count_trees(right_count, down_count)
+
+print(f'Part 1: {count_trees(3, 1)}')
+print(f'Part 2: {total_trees}')
