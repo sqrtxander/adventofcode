5ab30254176fc4d1eba57b5f560bbcf299b17bc2
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2020-12-05 17:28:14 +1100

    Update day05.py

--- 2020/day05.py
+++ 2020/day05.py
@@ -1,5 +1,4 @@
 data = [line for line in open('inputs/input05.txt').read().strip().split('\n')]
-print(data)
 
 seat_ids = []
 
@@ -7,6 +6,7 @@ for seat in data:
     row = int(seat[:7].replace('B', '1').replace('F', '0'), 2)
     col = int(seat[-3:].replace('R', '1').replace('L', '0'), 2)
     seat_ids.append(row * 8 + col)
+
 print(f'Part 1: {max(seat_ids)}')
 
 unknown_seats = [x for x in range(1033) if x not in seat_ids]
