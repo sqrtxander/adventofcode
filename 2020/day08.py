56f322d6a84499e88ea473ba2ee733892e0e90d9
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2020-12-08 19:21:53 +1100

    2020 08

---
+++ 2020/day08.py
@@ -0,0 +1,37 @@
+data = [[i, int(j)] for i, j in [x.split(' ') for x in open('inputs/input08.txt').read().strip().split('\n')]]
+
+
+def solve():
+    acc = pos = 0
+    v = [False] * len(data)
+    while pos < len(data):
+        if v[pos]:
+            return acc, False
+        v[pos] = True
+        inst, arg = data[pos]
+        if inst == 'acc':
+            acc += arg
+        elif inst == 'jmp':
+            pos += arg
+            continue
+
+        pos += 1
+
+    return acc, True
+
+
+print(f'Part 1: {solve()[0]}')
+
+
+for pos in range(len(data)):
+    if data[pos][0] != 'acc':
+        data[pos][0] = 'jmp' if data[pos][0] == 'nop' else 'nop'  # switch instruction
+        ans = solve()
+        data[pos][0] = 'jmp' if data[pos][0] == 'nop' else 'nop'  # switch back
+
+    else:
+        continue
+
+    if ans[1]:
+        print(f'Part 2: {ans[0]}')
+        break
