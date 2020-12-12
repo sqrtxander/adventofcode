b91fc26bafecd128226236c1b72aa0a1f7527b71
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2020-12-12 11:19:45 +1100

    2020 11

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,78 +1,92 @@
-import numpy as np
-import copy
+from copy import deepcopy
 
-test = '''L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL'''
-data = [list(x.strip()) for x in test.split('\n')]
-# data = [list(x.strip()) for x in open('inputs/input11.txt').readlines()]
-# print(np.matrix(data))
+data = [list(x.strip()) for x in open('inputs/input11.txt').readlines()]
 width = len(data[0])
 height = len(data)
-print(width, height)
 
 
-def change_to(x, y):
-    xl = x - 1 if x >= 1 else 0
-    yl = y - 1 if y >= 1 else 0
-    xu = x + 2 if x <= width - 2 else width
-    yu = y + 2 if y <= height - 2 else height
-
-    if data[y][x] == 'L':
-
-        for i in range(xl, xu):
-            for j in range(yl, yu):
-                if data[j][i] == '#' and i != x and j != y:
-                    return 'L'
-        return '#'
-    elif data[y][x] == '#':
-        occ_count = 0
-        for i in range(xl, xu):
-            for j in range(yl, yu):
-                if data[j][i] == '#' and i != x and j != y:
+def change_to_1(x, y):
+    occ_count = 0
+    for xx in (-1, 0, 1):
+        for yy in (-1, 0, 1):
+            if not(xx == 0 and yy == 0):
+                xp = x + xx
+                yp = y + yy
+                if 0 <= xp < width and 0 <= yp < height and data[yp][xp] == '#':
                     occ_count += 1
+    if data[y][x] == 'L':
+        return '#' if occ_count == 0 else 'L'
+    elif data[y][x] == '#':
         return 'L' if occ_count >= 4 else '#'
 
 
+def change_to_2(x, y):
+    occ_count = 0
+    for xd, yd in ((1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)):
+        xx = x
+        yy = y
+        xx += xd
+        yy += yd
+        if 0 <= xx < width and 0 <= yy < height and data[yy][xx] == '.':
+            xx += xd
+            yy += yd
+            if not(0 <= xx < width and 0 <= yy < height):
+                continue
+            while data[yy][xx] == '.':
+                xx += xd
+                yy += yd
+                if not(0 <= xx < width and 0 <= yy < height):
+                    break
+            if not(0 <= xx < width and 0 <= yy < height):
+                continue
+        if not(0 <= xx < width and 0 <= yy < height):
+            continue
+        if data[yy][xx] == '#':
+            occ_count += 1
+
+    if data[y][x] == 'L':
+        return '#' if occ_count == 0 else 'L'
+    elif data[y][x] == '#':
+        return 'L' if occ_count >= 5 else '#'
+
+
 while True:
-    # next_data = data.copy()
-    next_data = copy.deepcopy(data)
+    next_data = deepcopy(data)
     for x in range(width):
         for y in range(height):
             if data[y][x] != '.':
-                next_data[y][x] = change_to(x, y)
-    print(np.matrix(next_data))
+                next_data[y][x] = change_to_1(x, y)
 
     if data == next_data:
         break
-    # data = next_data[:]
-    data = copy.deepcopy(next_data)
+    data = deepcopy(next_data)
+
 occ_count = 0
-for x in range(width):
-    for y in range(height):
-        if next_data[y][x] == '#':
+for x in data:
+    for y in x:
+        if y == '#':
             occ_count += 1
 
-print(occ_count)
+print(f'Part 1: {occ_count}')
 
-# not 2131
+data = [list(x.strip()) for x in open('inputs/input11.txt').readlines()]
 
+while True:
+    next_data = deepcopy(data)
 
-def bounds(x, y):
-    xl = x - 1 if x >= 1 else 0
-    yl = y - 1 if y >= 1 else 0
-    xu = x + 2 if x <= width - 1 else width
-    yu = y + 2 if y <= height - 1 else height
-    return xl, yl, xu, yu
+    for x in range(width):
+        for y in range(height):
+            if data[y][x] != '.':
+                next_data[y][x] = change_to_2(x, y)
 
-# print(bounds(3, ))
+    if data == next_data:
+        break
+    data = deepcopy(next_data)
 
-for x in range(0, 2):
-    print(x)
+occ_count = 0
+for x in data:
+    for y in x:
+        if y == '#':
+            occ_count += 1
+
+print(f'Part 2: {occ_count}')
