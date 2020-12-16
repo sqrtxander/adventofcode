e236eb1270b57c79eca256584e60ec2cc87190f2
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2020-12-16 17:38:40 +1100

    2020 02 parsing refactor

--- 2020/day02.py
+++ 2020/day02.py
@@ -1,15 +1,14 @@
-data = [x.split(' ') for x in open('inputs/input02.txt').read().strip().split('\n')]
-for x in range(len(data)):
-    data[x][0] = [int(x) for x in data[x][0].split('-')]
-    data[x][1] = data[x][1][0]
+import re
+
+data = [(int(a), int(b), c, d) for a, b, c, d in re.findall(r'(\d+)-(\d+) (\w): (\w+)', open('inputs/input02.txt').read())]
 
 correct_pass_1, correct_pass_2 = 0, 0
-for rule, key, password in data:
+for rulel, ruleu, key, password in data:
     
-    if rule[0] <= password.count(key) <= rule[1]:
+    if rulel <= password.count(key) <= ruleu:
         correct_pass_1 += 1
 
-    if (password[rule[0]-1] == key) ^ (password[rule[1]-1] == key):
+    if (password[rulel-1] == key) ^ (password[ruleu-1] == key):
         correct_pass_2 += 1
 
 print(f'Part 1: {correct_pass_1}')
