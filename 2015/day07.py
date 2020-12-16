5559e9042adb05d875b03a67e83f2d697229ea34
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2020-12-16 17:43:26 +1100

    2015 07 still broken

--- 2015/day07.py
+++ 2015/day07.py
@@ -1,8 +1,42 @@
-data = [line.split(' -> ') for line in open('inputs/input07.txt').read().strip().split('\n')]
-print(data)
-for line in range(len(data)):
-    data[line][0] = data[line][0].split(' ')
+import re
 
-print(data)
+test = '''123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i'''
 
-# if
+# open('inputs/input07.txt').read()
+data = {}
+for line in test.strip().split('\n'):
+    command, key = line.split(' -> ')
+    data[key.strip()] = command
+
+
+def get_value(key):
+    try:
+        return int(key)
+    except ValueError:
+        pass
+
+    cmd = data[key].split(' ')
+
+    if 'AND' in cmd:
+        return get_value(cmd[0]) & get_value(cmd[2])
+    elif 'NOT' in cmd:
+        return ~get_value(cmd[1])
+    elif 'OR' in cmd:
+        return get_value(cmd[0]) | get_value(cmd[2])
+    elif 'RSHIFT' in cmd:
+        return get_value(cmd[0]) >> get_value(cmd[2])
+    elif 'LSHIFT' in cmd:
+        return get_value(cmd[0]) << get_value(cmd[2])
+    else:
+        return get_value(cmd[0])
+
+
+print(get_value('h'))
+print(~ 5)
