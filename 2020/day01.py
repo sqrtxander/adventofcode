e99e3f0758237ccaaafefeca9ea48ddbc03d6e52
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2021-08-29 20:06:10 +1000

    Update day01.py

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,15 +1,22 @@
-data = [int(x) for x in open('inputs/input01.txt').readlines()]
+import time
 
-for i in data:
-    if 2020 - i in data:
+with open('inputs/input01.txt', 'r') as f:
+    nums = set(int(x) for x in f.readlines())
+
+t = time.perf_counter()
+
+for i in nums:
+    if 2020 - i in nums:
         print(f'Part 1: {i * (2020 - i)}')
         break
 
-for i in data:
-    for j in data[data.index(i) + 1:]:
-        if 2020 - i - j in data:
+for i in nums:
+    for j in nums:
+        if 2020 - i - j in nums:
             print(f'Part 2: {i * (2020 - i - j) * j}')
             break
     else:
         continue
     break
+
+print(time.perf_counter() - t)
