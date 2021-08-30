a4f8187db48bd15d5c63359ba257c2acd410f3ad
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2021-08-30 15:42:26 +1000

    day09 complete

--- 2015/day09.py
+++ 2015/day09.py
@@ -1,6 +1,24 @@
-data = [line.split(' = ') for line in open('inputs/input09.txt').read().strip().split('\n')]
-print(data)
-for x in range(len(data)):
-    data[x][0] = data[x][0].split(' to ')
-    data[x][1] = int(data[x][1])
-print(data)
+import itertools
+
+locations = set()
+distances = dict()
+
+with open('inputs/input09.txt', 'r') as f:
+    for line in f.readlines():
+        # print(line)
+        loc1, _, loc2, _, dist = line.split()
+        locations.add(loc1)
+        locations.add(loc2)
+        distances[(loc1, loc2)] = int(dist)
+        distances[(loc2, loc1)] = int(dist)
+
+all_distances = []
+for path in itertools.permutations(locations):
+    # print(path)
+    dist = 0
+    for i in range(len(path)-1):
+        dist += distances[(path[i], path[i + 1])]
+    all_distances.append(dist)
+
+print(min(all_distances))
+print(max(all_distances))
