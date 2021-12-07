ff0de8274300eabf717588e5077f0669e8c8708e
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2021-12-07 17:13:38 +1100

    day07 python complete

---
+++ 2021/day07/python/part2.py
@@ -0,0 +1,11 @@
+def count_fuel(pos):
+    return sum(abs(num-pos)*(abs(num-pos)+1)//2 for num in positions)
+
+
+if __name__ == '__main__':
+    with open('../input.txt', 'r') as f:
+        positions = [int(num) for num in f.read().split(',')]
+
+    fuel_cost = {count_fuel(i) for i in range(min(positions), max(positions) + 1)}
+
+    print(min(fuel_cost))
