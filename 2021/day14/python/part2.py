c29ea8e5a7e7d176b1bc304c642a67ab1e85c463
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2021-12-16 15:31:27 +1100

    day 14 python complete

---
+++ 2021/day14/python/part2.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+
+if __name__ == '__main__':
+    with open('../input.txt', 'r') as f:
+        polymer, pairs = f.read().split('\n\n')
+        polymer_pairs = Counter([a + b for a, b in zip(polymer, polymer[1:])])
+
+        pairs = pairs.split('\n')
+        replacements = {}
+        for i, line in enumerate(pairs):
+            a, b = line.split(' -> ')
+            replacements[a] = b
+
+    new_polymer_pairs = polymer_pairs.copy()
+    letter_count = Counter(polymer)
+    for _ in range(40):
+        for rep in replacements:
+            a, b = rep[0], rep[1]
+            c = replacements[rep]
+            count = polymer_pairs[rep]
+            new_polymer_pairs[a + c] += count
+            new_polymer_pairs[c + b] += count
+            new_polymer_pairs[rep] -= count
+            letter_count[c] += count
+        polymer_pairs = new_polymer_pairs.copy()
+
+    print(max(letter_count.values()) - min(letter_count.values()))
