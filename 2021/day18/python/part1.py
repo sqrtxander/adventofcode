475fe4f9781b9e4fe778c8e0e1f7073b56546f2e
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-07-16 16:32:57 +1000

    2021 day 18 start not yet solved

--- 2021/day18/python/part1.py
+++ 2021/day18/python/part1.py
@@ -1,9 +1,3 @@
-def parse(file):
-    with open(file, 'r') as f:
-        data = [eval(line) for line in f.read().splitlines()]
-    return data
-
-
 def magnitude(num):
     if type(num) == int:
         return num
@@ -11,6 +5,17 @@ def magnitude(num):
         return 3 * magnitude(num[0]) + 2 * magnitude(num[1])
     else:
         raise
+
+
+def find_explode(num, depth=0):
+    if type(num) == list:
+        if depth == 4:
+            return True
+        for i in num:
+            e = find_explode(i, depth+1)
+            if e:
+                return i
+    return False
 
 
 def explode(num, depth=0):
@@ -23,9 +28,13 @@ def explode(num, depth=0):
                 return explode(i, depth+1)
 
 
-def solve(data):
+
+
+def solve(file):
+    with open(file, 'r') as f:
+        snail_nums = [eval(line) for line in f.read().splitlines()]
     total = []
-    for snail_num in data:
+    for snail_num in snail_nums:
         total += [snail_num]
     return magnitude(total)
 
@@ -34,12 +43,16 @@ def solve(data):
 if __name__ == '__main__':
 
     # EXPECTED = 4140
-    # test = solve(parse('../test.in'))
+    # test = solve('../test.in')
     # assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
-    # print(solve(parse('../input.in')))
-    print(solve([[1,2], [[3,4],5]]))
+    # print(solve('../input.in'))
+    # print(solve([[1,2], [[3,4],5]]))
 
-    print(explode([[[[[9,8],1],2],3],4]))
-    print(explode([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]))
-    print(explode([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]))
-    print(str([1, [2, 3]]))
+    # print(magnitude([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]))
+
+    print(find_explode([[[[0,7],4],[7,[[8,4],9]]],[1,1]]))
+    print(find_explode([[[[0,7],4],[15,[0,13]]],[1,1]]))
+    # print(explode([[[[[9,8],1],2],3],4]))
+    # print(explode([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]))
+    # print(explode([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]))
+    # print(str([1, [2, 3]]))
