55df89b41ab2d06d1d280b057225b17a37262e78
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-07-16 16:32:36 +1000

    2021 day 1 update and boilerplate update

--- 2021/day00/python/part2.py
+++ 2021/day00/python/part2.py
@@ -1,16 +1,12 @@
-def parse(file):
+def solve(file):
     with open(file, 'r') as f:
         data = [line for line in f.read().splitlines()]
-    return data
-
-
-def solve(data):
     return data
 
 
 if __name__ == '__main__':
 
     EXPECTED = 0
-    test = solve(parse('../test.in'))
+    test = solve('../test.in')
     assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
-    print(solve(parse('../input.in')))
+    print(solve('../input.in'))
--- 2021/day01/python/part1.py
+++ 2021/day01/python/part1.py
@@ -1,16 +1,11 @@
-def parse(file):
+def solve(file):
     with open(file, 'r') as f:
-        data = [int(x) for x in f.readlines()]
-    return data
-
-
-def solve(depths):
+        depths = [int(x) for x in f.readlines()]
     return sum(depths[i] > depths[i-1] for i in range(1, len(depths)))
 
 
 if __name__ == '__main__':
-
     EXPECTED = 7
-    test = solve(parse('../test.in'))
+    test = solve('../test.in')
     assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
-    print(solve(parse('../input.in')))
+    print(solve('../input.in'))
