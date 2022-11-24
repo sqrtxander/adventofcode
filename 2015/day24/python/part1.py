a26d6b4de916038b1176a716a5ae8cfc62c5e317
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-11-24 17:04:46 +1100

    2015 day 24 complete

---
+++ 2015/day24/python/part1.py
@@ -0,0 +1,29 @@
+from functools import reduce
+import itertools
+
+
+def solve(file):
+    with open(file, 'r') as f:
+        weights = set(int(line) for line in f.read().splitlines())
+    
+    individual_weight = sum(weights) // 3    
+    done = False
+    smallest = float('inf')
+
+    for i in range(1, len(weights)):
+        for combo in itertools.combinations(weights, i):
+            if sum(combo) == individual_weight:
+                res = reduce(lambda x, y: x * y, combo)
+                if res < smallest:
+                    smallest = res
+                done = True
+        if done:
+            break
+    return smallest
+
+if __name__ == '__main__':
+
+    EXPECTED = 99
+    test = solve('../test.in')
+    assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
+    print(solve('../input.in'))
