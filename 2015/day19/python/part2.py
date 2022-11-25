6bf96565686dd80db43419e70ddb5b0987cddc05
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-11-25 11:41:35 +1100

    2015 day 19 complete

--- 2015/day19/python/part2.py
+++ 2015/day19/python/part2.py
@@ -1,54 +1,31 @@
 import re
 
 
-def replace_nth(string, sub, replace, n):
-    find = string.find(sub)
-    i = find != -1
-    while find != -1 and i != n:
-        find = string.find(sub, find + 1)
-        i += 1
-    if i == n:
-        return string[:find] + replace + string[find + len(sub):]
-    return string
+def solve(file):
+    def replace(x):
+        return replacements[x.group()]
+
+    with open(file, 'r') as f:
+        replacements = re.findall(r'(\w+) => (\w+)', f.read())
+        f.seek(0)
+        molecule = f.readlines()[-1][::-1]
+        replacements = {x[1][::-1]: x[0][::-1] for x in replacements}
 
 
-def next_iteration(molecule, depth=1):
-    molecules = set()
-    for x, y in replacements:
-        for i in range(1, molecule.count(y) + 1):
-            new = replace_nth(molecule, y, x, i)
-            if new not in starters:
-                molecules.add(new)
-            else:
-                print('SUCCESS')
-                print(depth+1)
-                exit()
-    for mol in molecules:
-        next_iteration(mol, depth+1)
+    count = 0
+    while molecule not in ('e', 'ee'):
+        molecule = re.sub('|'.join(replacements.keys()), replace, molecule, 1)
+        # print(molecule)
+        count += 1
 
+    return count
 
-def part_2(molecule, replacements):
-    depth = 1
-    while molecule not in starters:
-        for reactant, product in replacements:
-            if product in molecule:
-                molecule = molecule.replace(product, reactant)
-                print(molecule)
-                depth += 1
-
-    return depth
 
 
 if __name__ == '__main__':
-    with open('../input.txt', 'r') as f:
-        replacements = re.findall(r'(\w+) => (\w+)', f.read())
-        f.seek(0)
-        molecule = f.readlines()[-1]
-        # molecule = 'HOHOHO'
-        starters = [x[1] for x in replacements if x[0] == 'e']
-        replacements = [x for x in replacements if x[0] != 'e']
 
-    print(part_2(molecule, replacements))
-    # print(next_iteration(molecule))
-
-    # unsure how to continue
+    EXPECTED = 6
+    test = solve('../test.in')
+    assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
+    print('solved test case')
+    print(solve('../input.in'))
