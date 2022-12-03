ad4b491c39514807b1eed93f163c907872618486
sqrtxander <72635221+sqrtxander@users.noreply.github.com>
2022-12-03 17:19:58 +1100

    2022 day03 overhaul

--- 2022/day03/part1.py
+++ 2022/day03/part1.py
@@ -1,27 +1,45 @@
-def solve(file):
+import os
+
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+EXPECTED = 157
+TEST_INPUT = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+
+
+def solve(inp):
     def common_letter(str1, str2):
-        return set(str1).intersection(str2).pop() 
+        return set(str1).intersection(str2).pop()
 
-    with open(file, 'r') as f:
-        data = [line for line in f.read().splitlines()]
+    data = [line for line in inp.strip().splitlines()]
 
-    total_priorities = 0 
+    total_priorities = 0
 
     for line in data:
         first_half = line[:len(line)//2]
         second_half = line[len(line)//2:]
         badge = common_letter(first_half, second_half)
-        if badge.islower():          
+        if badge.islower():
             total_priorities += ord(badge) - 96
         else:
-            total_priorities +=  ord(badge) - 38    
+            total_priorities += ord(badge) - 38
 
     return total_priorities
 
 
-if __name__ == '__main__':
-
-    EXPECTED = 157
-    test = solve('test.in')
+def main():
+    test = solve(TEST_INPUT)
     assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
-    print(solve('input.in'))
+
+    with open(INPUT_TXT, 'r') as f:
+        print(solve(f.read()))
+
+
+if __name__ == '__main__':
+    main()
