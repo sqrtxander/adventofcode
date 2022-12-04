3884b048f013ea109da3992c5e9a7fed1c507656
sqrtxander <xanderjames128@gmail.com>
2022-12-04 16:17:40 +1100

    2022 day04

---
+++ 2022/day04/part1.py
@@ -0,0 +1,35 @@
+import os
+
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+EXPECTED = 2
+TEST_INPUT = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+
+
+def solve(inp):
+    def fully_contained(range1, range2):
+        return range1[0] >= range2[0] and range1[1] <= range2[1] or range2[0] >= range1[0] and range2[1] <= range1[1]
+
+    data = [[[int(num) for num in part.split('-')]
+             for part in line.split(',')] for line in inp.strip().splitlines()]
+
+    return sum(fully_contained((a, b), (c, d)) for (a, b,), (c, d) in data)
+
+
+def main():
+    test = solve(TEST_INPUT)
+    assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
+
+    with open(INPUT_TXT, 'r') as f:
+        print(solve(f.read()))
+
+
+if __name__ == '__main__':
+    main()
