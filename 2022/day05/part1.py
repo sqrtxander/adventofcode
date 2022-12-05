11743d33420171b6321cfbce26a8e35e6938ad36
sqrtxander <xanderjames128@gmail.com>
2022-12-05 17:03:19 +1100

    2022 day05

---
+++ 2022/day05/part1.py
@@ -0,0 +1,47 @@
+import os
+import re
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+EXPECTED = 'CMZ'
+TEST_INPUT = '''\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+'''
+
+
+def solve(inp):
+    init, moves = inp.split('\n\n')
+
+    towers = [[] for _ in range((len(init.splitlines()[-1]) + 2) // 4)]
+
+    for line in init.splitlines()[:-1]:
+        for i, char in enumerate(line):
+            if char.isalpha():
+                towers[(i+2)//4].append(char)
+
+    for line in moves.splitlines():
+        count, from_tower, to_tower = (int(num)
+                                       for num in re.findall(r'\d+', line))
+        for _ in range(count):
+            towers[to_tower - 1].insert(0, towers[from_tower - 1].pop(0))
+
+    return ''.join(tower[0] for tower in towers)
+
+
+def main():
+    test = solve(TEST_INPUT)
+    assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
+
+    with open(INPUT_TXT, 'r') as f:
+        print(solve(f.read()))
+
+
+if __name__ == '__main__':
+    main()
