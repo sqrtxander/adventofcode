e1c37f79660bc720593ac957f1439639544c11cb
sqrtxander <xanderjames128@gmail.com>
2022-12-10 22:50:42 +1100

    2022 day10

---
+++ 2022/day10/part1.py
@@ -0,0 +1,181 @@
+import os
+
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+EXPECTED = 13140
+TEST_INPUT = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+
+
+def solve(inp):
+    data = inp.strip().splitlines()
+
+    x = 1
+    xs = [x]
+    for line in data:
+        if line == 'noop':
+            xs.append(x)
+        elif line.startswith('addx'):
+            xs.append(x)
+            x += int(line.split(' ')[1])
+            xs.append(x)
+
+    return sum(xs[i] * (i + 1) for i in range(19, 220, 40))
+
+
+def main():
+    test = solve(TEST_INPUT)
+    assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
+
+    with open(INPUT_TXT, 'r') as f:
+        print(solve(f.read()))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2022/day10/part2.py
@@ -0,0 +1,196 @@
+import os
+
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.in')
+EXPECTED = '''\
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....'''
+TEST_INPUT = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+
+
+def solve(inp):  # sourcery skip: assign-if-exp
+    data = inp.strip().splitlines()
+
+    x = 1
+    xs = [x]
+    for line in data:
+        if line == 'noop':
+            xs.append(x)
+        elif line.startswith('addx'):
+            xs.append(x)
+            x += int(line.split(' ')[1])
+            xs.append(x)
+
+    answer = ''
+    for i, val in enumerate(xs):
+        if i % 40 - 1 <= val <= i % 40 + 1:
+            answer += '#'
+        else:
+            answer += '.'
+        if i % 40 == 39:
+            answer += '\n'
+
+    return answer[:-2]
+
+
+def main():
+    test = solve(TEST_INPUT)
+    assert test == EXPECTED, f'Got {test} should be {EXPECTED}'
+
+    with open(INPUT_TXT, 'r') as f:
+        print(solve(f.read()))
+
+
+if __name__ == '__main__':
+    main()
